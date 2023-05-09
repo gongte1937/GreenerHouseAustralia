bdbffb273f6950a6c698cf81b30ac468b84b93f8
Terry <gongte1937@gmail.com>
2023-05-09 19:30:28 +0930

    Revert "remove next and change to vite"
    This reverts commit a91b59f60569697f284a7ee38d7ce0b98a2188b9.

---
+++ green-house-aus/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Main() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+
+    </div>
+  )
+}
