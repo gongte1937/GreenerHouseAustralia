ac917fd8135802af646e495b8a10dbbc30fe87d1
Terry <gongte1937@gmail.com>
2023-05-09 19:57:56 +0930

    use vite framework

---
+++ greener-house-aus/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import {Container, Navbar as NavbarBs } from "react-bootstrap" 
+
+const NavBar = () => {
+    return (<NavbarBs>
+        <Container>
+            
+        </Container>
+        </NavbarBs>);
+}
+ 
+export default NavBar;
