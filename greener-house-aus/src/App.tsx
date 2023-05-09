e7b2bdcb0201ba42eea9296bbc5eb3fb3983a093
Terry <gongte1937@gmail.com>
2023-05-09 19:58:06 +0930

    use vite

---
+++ greener-house-aus/src/App.tsx
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap"
+import {Routes, Route} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+
+function App() {
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<HomePage/>}> /</Route>
+        <Route path="/about" element={<AboutPage/>}> /</Route>
+        <Route path="/contact" element={<ContactPage/>}> /</Route>
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
